bce5d428e6d2d379ef99b272f88746090fa4e6a9
BenedictUmeozor <benedictumeozor@gmail.com>
2023-08-27 13:26:22 +0100

    lazy load image

---
+++ src/components/LazyImage.jsx
@@ -0,0 +1,37 @@
+import { useRef, useEffect, useState } from "react";
+
+const LazyImage = ({ src, alt, className }) => {
+  const imgRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting) {
+        setIsVisible(true);
+        observer.unobserve(entry.target);
+      }
+    });
+
+    if (imgRef.current) {
+      observer.observe(imgRef.current);
+    }
+
+    return () => {
+      if (imgRef.current) {
+        observer.unobserve(imgRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <img
+      ref={imgRef}
+      src={isVisible ? src : ""}
+      alt={alt}
+      className={className}
+    />
+  );
+};
+
+export default LazyImage;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,21 +1,33 @@
 import { Link } from "react-router-dom";
 import post from "../assets/post.jpg";
+import LazyImage from "./LazyImage";
 
 const Post = () => {
   return (
     <div className="p-2 max-md:mb-5">
       <div className="h-52 w-full relative overflow-hidden">
-        <img src={post} className="w-full h-full rounded object-cover" alt="" />
+        <LazyImage
+          src={post}
+          className={"w-full h-full rounded object-cover"}
+          alt={post}
+        />
         <div className="absolute top-0 left-0 h-full w-full bg-black opacity-20 transition-all duration-150 hover:opacity-0 rounded"></div>
       </div>
       <div className="mt-4">
-        <Link className="font-semibold text-xl mb-3 hover:underline inline-block">Lorem ipsum dolor sit amet consectetur.</Link>
+        <Link className="font-semibold text-xl mb-3 hover:underline inline-block">
+          Lorem ipsum dolor sit amet consectetur.
+        </Link>
         <p className="text-small text-gray-800 mb-2">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
           aspernatur dolores explicabo magni minus non sit consequuntur quasi
           libero quibusdam!
         </p>
-        <p className="text-small mb-2">By <Link className="text-emerald-500 hover:underline" to="/admin">Admin</Link></p>
+        <p className="text-small mb-2">
+          By{" "}
+          <Link className="text-emerald-500 hover:underline" to="/admin">
+            Admin
+          </Link>
+        </p>
         <span className="text-xs text-gray-600">August 25, 2023</span>
       </div>
     </div>
